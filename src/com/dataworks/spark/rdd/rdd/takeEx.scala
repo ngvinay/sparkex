ec6221d7fe3e543f69ed0fd88f1dc4514657e6cb
Sandeep KS <sandeep.ksetty@gmail.com>
2016-10-31 16:11:08 -0400

    take Example is added

---
+++ src/com/dataworks/spark/rdd/rdd/takeEx.scala
@@ -0,0 +1,25 @@
+package com.dataworks.spark.rdd.rdd
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+  * Created by Sandeep on 10/31/16.
+  */
+object takeEx {
+  def main(args: Array[String]): Unit = {
+    val conf: SparkConf = new SparkConf().setAppName("foldEx").setMaster("local")
+    val sc: SparkContext = new SparkContext(conf)
+    sc.setLogLevel("OFF")
+
+    takeInts(sc)
+  }
+
+  def takeInts(sc: SparkContext): Unit = {
+    val baseRdd: RDD[Int] = sc.parallelize(1 to 100, 10)
+    val collectedArray: Array[Int] = baseRdd.take(28)
+
+    collectedArray.foreach(x => println(x))
+  }
+
+}
