323ed9fe6907b8fbf1e2306c0b38e5622b8f42b2
Sandeep KS <sandeep.ksetty@gmail.com>
2016-10-31 16:03:02 -0400

    Collect Ex added

--- src/com/dataworks/spark/rdd/rdd/collectEx.scala
+++ src/com/dataworks/spark/rdd/rdd/collectEx.scala
@@ -1,8 +1,26 @@
 package com.dataworks.spark.rdd.rdd
 
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
 /**
   * Created by Sandeep on 10/28/16.
   */
 object collectEx {
+
+  def main(args: Array[String]): Unit = {
+    val conf: SparkConf = new SparkConf().setAppName("foldEx").setMaster("local")
+    val sc: SparkContext = new SparkContext(conf)
+    sc.setLogLevel("OFF")
+
+    collectInts(sc)
+  }
+
+  def collectInts(sc: SparkContext): Unit = {
+    val baseRdd: RDD[Int] = sc.parallelize(1 to 100)
+    val collectedArray: Array[Int] = baseRdd.collect()
+
+    collectedArray.foreach(x => println(x))
+  }
 
 }
