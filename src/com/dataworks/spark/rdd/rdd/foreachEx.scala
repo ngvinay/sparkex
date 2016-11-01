b5fa3ff099fc21da3322a6032b35484668bc6320
Sandeep KS <sandeep.ksetty@gmail.com>
2016-11-01 13:50:07 -0400

    forEach Example

--- src/com/dataworks/spark/rdd/rdd/foreachEx.scala
+++ src/com/dataworks/spark/rdd/rdd/foreachEx.scala
@@ -1,8 +1,24 @@
 package com.dataworks.spark.rdd.rdd
 
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
 /**
   * Created by Sandeep on 10/28/16.
   */
 object foreachEx {
+
+  def main(args: Array[String]): Unit = {
+    val conf: SparkConf = new SparkConf().setAppName("foreachEx").setMaster("local")
+    val sc: SparkContext = new SparkContext(conf)
+    sc.setLogLevel("OFF")
+
+    forEachPrint(sc)
+  }
+
+  def forEachPrint(sc: SparkContext): Unit = {
+    val baseRdd: RDD[Int] = sc.parallelize(1 to 100)
+    baseRdd.foreach(x => println(x))
+  }
 
 }
