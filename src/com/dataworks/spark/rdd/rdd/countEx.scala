078c4433398615e46e12c891b508c4c349938d0b
Sandeep KS <sandeep.ksetty@gmail.com>
2016-11-08 13:18:00 -0500

    Filter case is added

--- src/com/dataworks/spark/rdd/rdd/countEx.scala
+++ src/com/dataworks/spark/rdd/rdd/countEx.scala
@@ -1,8 +1,21 @@
 package com.dataworks.spark.rdd.rdd
 
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
 /**
   * Created by Sandeep on 10/28/16.
   */
 object countEx {
+  def main(args: Array[String]): Unit = {
+    val conf: SparkConf = new SparkConf().setAppName("countEx").setMaster("local")
+    val sc: SparkContext = new SparkContext(conf)
+    sc.setLogLevel("OFF")
+
+    val baseRdd: RDD[Int] = sc.parallelize(1 to 100, 10)
+    val mappedRdd: RDD[Int] = baseRdd.map(x => x + 2)
+
+    println("Count:" + mappedRdd.count())
+  }
 
 }
