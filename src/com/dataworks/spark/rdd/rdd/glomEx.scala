b0d393f1f9aca26932d1b456aa55200a54d71b9f
Sandeep KS <sandeep.ksetty@gmail.com>
2016-11-08 14:32:04 -0500

    Initial Functionality Check-in

--- src/com/dataworks/spark/rdd/rdd/glomEx.scala
+++ src/com/dataworks/spark/rdd/rdd/glomEx.scala
@@ -1,8 +1,21 @@
 package com.dataworks.spark.rdd.rdd
 
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
 /**
   * Created by Sandeep on 10/28/16.
   */
 object glomEx {
+  def main(args: Array[String]): Unit = {
+    val conf: SparkConf = new SparkConf().setAppName("glomEx").setMaster("local")
+    val sc: SparkContext = new SparkContext(conf)
+    sc.setLogLevel("OFF")
+
+    val baseRdd: RDD[Int] = sc.parallelize(Seq(100, 1, 4, 5, 2, 99, 3, 88, 1000, 34, 24, 12, 10, 5, 0), 3)
+    val maxElement: Int = baseRdd.glom().map(x => x.max).reduce((x, y) => x max y)
+
+    println("Max in the Seq:" + maxElement)
+  }
 
 }
