4d43c5cbf473abebe5aab7cc26ff76eda627e750
Sandeep KS <sandeep.ksetty@gmail.com>
2016-11-08 14:05:16 -0500

    Initial Functionality Check-in

--- src/com/dataworks/spark/rdd/rdd/dependenciesEx.scala
+++ src/com/dataworks/spark/rdd/rdd/dependenciesEx.scala
@@ -1,8 +1,25 @@
 package com.dataworks.spark.rdd.rdd
 
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{Dependency, SparkConf, SparkContext}
+
 /**
   * Created by Sandeep on 10/28/16.
   */
 object dependenciesEx {
+  def main(args: Array[String]): Unit = {
+    val conf: SparkConf = new SparkConf().setAppName("dependenciesEx").setMaster("local")
+    val sc: SparkContext = new SparkContext(conf)
+    sc.setLogLevel("OFF")
+
+    val baseRdd: RDD[Int] = sc.parallelize(1 to 100, 10)
+    val mappedRdd: RDD[Int] = baseRdd.map(x => x + 2)
+    val repartRdd: RDD[Int] = mappedRdd.repartition(5)
+    val dependencies: Seq[Dependency[_]] = repartRdd.dependencies
+
+
+    repartRdd.dependencies.map(_.rdd).foreach(println)
+    println(repartRdd.toDebugString)
+  }
 
 }
