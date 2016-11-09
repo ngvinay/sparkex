3af6ab0f9cc6c0d22a9c4ffdf3b83fa3f922f80a
Sandeep KS <sandeep.ksetty@gmail.com>
2016-11-09 12:59:08 -0500

    Initial Functionality Check-in

--- src/com/dataworks/spark/rdd/rdd/mapPartitionsWithIndexEx.scala
+++ src/com/dataworks/spark/rdd/rdd/mapPartitionsWithIndexEx.scala
@@ -1,8 +1,34 @@
 package com.dataworks.spark.rdd.rdd
 
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
 /**
   * Created by Sandeep on 10/28/16.
   */
 object mapPartitionsWithIndexEx {
+  def main(args: Array[String]): Unit = {
+    val conf: SparkConf = new SparkConf().setAppName("mapPartitionsWithIndexEx").setMaster("local")
+    val sc: SparkContext = new SparkContext(conf)
+    sc.setLogLevel("OFF")
+
+    mapPartitionsIndxStr(sc)
+  }
+
+  def mapPartitionsIndxStr(sc: SparkContext): Unit = {
+    val baseRDD: RDD[String] = sc.textFile("/Users/Sandeep/dataworks/data/spark", 4)
+    val filteredRdd: RDD[String] = baseRDD.filter(line => !line.isEmpty)
+    val flattenedRdd: RDD[String] = filteredRdd.flatMap(line => line.split(" "))
+    val mapPartitions: RDD[(String, Int)] = flattenedRdd.mapPartitionsWithIndex(
+      (index: Int, partition: Iterator[String]) => {
+        var pair: List[(String, Int)] = List[(String, Int)]()
+        partition.foreach(string => {
+          pair = pair :+ (string, index)
+        })
+        pair.iterator
+      })
+
+    mapPartitions.foreach(println)
+  }
 
 }
