048a2821cd307d39d703932d7366eeeede3e3df1
Sandeep KS <sandeep.ksetty@gmail.com>
2016-11-09 13:10:59 -0500

    Initial Functionality Check-in

--- src/com/dataworks/spark/rdd/rdd/maxEx.scala
+++ src/com/dataworks/spark/rdd/rdd/maxEx.scala
@@ -1,8 +1,26 @@
 package com.dataworks.spark.rdd.rdd
 
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
 /**
   * Created by Sandeep on 10/28/16.
   */
 object maxEx {
+
+  def main(args: Array[String]): Unit = {
+    val conf: SparkConf = new SparkConf().setAppName("maxEx").setMaster("local")
+    val sc: SparkContext = new SparkContext(conf)
+    sc.setLogLevel("OFF")
+
+    maxInt(sc)
+  }
+
+  def maxInt(sc: SparkContext): Unit = {
+    val baseRdd: RDD[Int] = sc.parallelize(1 to 25)
+    val max: Int = baseRdd.max()
+
+    println("Max: " + max)
+  }
 
 }
--- src/com/dataworks/spark/rdd/rdd/minEx.scala
+++ src/com/dataworks/spark/rdd/rdd/minEx.scala
@@ -1,8 +1,25 @@
 package com.dataworks.spark.rdd.rdd
 
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
 /**
   * Created by Sandeep on 10/28/16.
   */
 object minEx {
+  def main(args: Array[String]): Unit = {
+    val conf: SparkConf = new SparkConf().setAppName("maxEx").setMaster("local")
+    val sc: SparkContext = new SparkContext(conf)
+    sc.setLogLevel("OFF")
+
+    minInt(sc)
+  }
+
+  def minInt(sc: SparkContext): Unit = {
+    val baseRdd: RDD[Int] = sc.parallelize(1 to 25)
+    val min: Int = baseRdd.min()
+
+    println("Min: " + min)
+  }
 
 }
