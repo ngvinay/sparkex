605444970516488921d93a00163468d71740c419
Sandeep KS <sandeep.ksetty@gmail.com>
2016-11-09 14:32:44 -0500

    Initial Functionality Check-in

--- src/com/dataworks/spark/rdd/rdd/sortByEx.scala
+++ src/com/dataworks/spark/rdd/rdd/sortByEx.scala
@@ -1,8 +1,28 @@
 package com.dataworks.spark.rdd.rdd
 
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
 /**
   * Created by Sandeep on 10/28/16.
   */
 object sortByEx {
+
+  def main(args: Array[String]): Unit = {
+    val conf: SparkConf = new SparkConf().setAppName("sortByEx").setMaster("local")
+    val sc: SparkContext = new SparkContext(conf)
+    sc.setLogLevel("OFF")
+
+    sortByStr(sc)
+  }
+
+  def sortByStr(sc: SparkContext): Unit = {
+    val baseRDD: RDD[String] = sc.textFile("/Users/Sandeep/dataworks/data/spark", 4)
+    val filteredRdd: RDD[String] = baseRDD.filter(line => !line.isEmpty)
+    val flattenedRdd: RDD[String] = filteredRdd.flatMap(line => line.split(" "))
+    val descendingRdd: RDD[String] = flattenedRdd.sortBy(word => word(0), ascending = false, 3)
+    descendingRdd.glom().map(x => x.mkString(", ")).foreach(println)
+
+  }
 
 }
