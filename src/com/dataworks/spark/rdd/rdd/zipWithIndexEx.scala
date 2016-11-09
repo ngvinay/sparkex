5c18f3a1097b9cdb619229914d3d34d3785e5572
Sandeep KS <sandeep.ksetty@gmail.com>
2016-11-09 15:55:08 -0500

    Initial Functionality Check-in

--- src/com/dataworks/spark/rdd/rdd/zipWithIndexEx.scala
+++ src/com/dataworks/spark/rdd/rdd/zipWithIndexEx.scala
@@ -1,8 +1,33 @@
 package com.dataworks.spark.rdd.rdd
 
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
 /**
   * Created by Sandeep on 10/28/16.
   */
 object zipWithIndexEx {
+  def main(args: Array[String]): Unit = {
+    val conf: SparkConf = new SparkConf().setAppName("zipWithIndexEx").setMaster("local")
+    val sc: SparkContext = new SparkContext(conf)
+    sc.setLogLevel("OFF")
+
+    zipIntStrPartn(sc)
+  }
+
+  def zipIntStrPartn(sc: SparkContext) = {
+    val baseRDD: RDD[String] = sc.textFile("/Users/Sandeep/dataworks/data/spark", 4)
+    val filteredRdd: RDD[String] = baseRDD.filter(line => !line.isEmpty)
+    val flattenedRdd: RDD[String] = filteredRdd.flatMap(line => line.split(" "))
+
+
+    println("Base RDD Partition cnt:" + baseRDD.partitions.length)
+
+    val zippedRdd: RDD[(String, Long)] = flattenedRdd.zipWithIndex()
+    zippedRdd.glom().map(x => x.mkString(", ")).foreach(println)
+
+  }
+
+
 
 }
