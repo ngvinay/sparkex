852efd74c775adc2bcf865102d4514d1fce92811
Sandeep KS <sandeep.ksetty@gmail.com>
2016-11-09 14:18:15 -0500

    Initial Functionality Check-in

--- src/com/dataworks/spark/rdd/rdd/coalesceEx.scala
+++ src/com/dataworks/spark/rdd/rdd/coalesceEx.scala
@@ -1,8 +1,27 @@
 package com.dataworks.spark.rdd.rdd
 
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
 /**
   * Created by Sandeep on 10/28/16.
   */
 object coalesceEx {
+  def main(args: Array[String]): Unit = {
+    val conf: SparkConf = new SparkConf().setAppName("coalesceEx").setMaster("local")
+    val sc: SparkContext = new SparkContext(conf)
+    sc.setLogLevel("OFF")
+
+    coalesce(sc)
+  }
+
+  def coalesce(sc: SparkContext): Unit = {
+    val baseRdd: RDD[Int] = sc.parallelize(1 to 100, 5)
+    println("Base RDD Partition size :" + baseRdd.partitions.length)
+
+    val repartRdd: RDD[Int] = baseRdd.coalesce(2, true)
+    println("Coalesced RDD Partition size :" + repartRdd.partitions.length)
+    println(repartRdd.toDebugString)
+  }
 
 }
