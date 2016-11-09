fce2dff06f0353abe9e6a02f6dc3e1686d41329e
Sandeep KS <sandeep.ksetty@gmail.com>
2016-11-09 14:13:14 -0500

    Initial Functionality Check-in

--- src/com/dataworks/spark/rdd/rdd/repartitionEx.scala
+++ src/com/dataworks/spark/rdd/rdd/repartitionEx.scala
@@ -1,8 +1,28 @@
 package com.dataworks.spark.rdd.rdd
 
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
 /**
   * Created by Sandeep on 10/28/16.
   */
 object repartitionEx {
+
+  def main(args: Array[String]): Unit = {
+    val conf: SparkConf = new SparkConf().setAppName("repartitionEx").setMaster("local")
+    val sc: SparkContext = new SparkContext(conf)
+    sc.setLogLevel("OFF")
+
+    repart(sc)
+  }
+
+  def repart(sc: SparkContext): Unit = {
+    val baseRdd: RDD[Int] = sc.parallelize(1 to 100, 5)
+    println("Base RDD Partition size :" + baseRdd.partitions.length)
+
+    val repartRdd: RDD[Int] = baseRdd.repartition(2)
+    println("Repartition RDD Partition size :" + repartRdd.partitions.length)
+    println(repartRdd.toDebugString)
+  }
 
 }
