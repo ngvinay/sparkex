6e90c33b14e19613e6951226de6f262521c9eb2b
Sandeep KS <sandeep.ksetty@gmail.com>
2016-11-15 13:34:35 -0500

    Initial Functionality Check-in

---
+++ src/com/dataworks/spark/rdd/pair/sortByKey.scala
@@ -0,0 +1,29 @@
+package com.dataworks.spark.rdd.pair
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+  * Created by Sandeep on 11/15/16.
+  */
+object sortByKey {
+  def main(args: Array[String]): Unit = {
+    val conf: SparkConf = new SparkConf().setAppName("sortByKey").setMaster("local")
+    val sc: SparkContext = new SparkContext(conf)
+    sc.setLogLevel("OFF")
+
+    sortKeyStr(sc)
+  }
+
+  def sortKeyStr(sc: SparkContext): Unit = {
+    val baseRDD: RDD[String] = sc.textFile("/Users/Sandeep/dataworks/data/spark")
+    val filteredRdd: RDD[String] = baseRDD.filter(line => !line.isEmpty).flatMap(_.split(" "))
+      .filter(x => !x.isEmpty)
+    val pairRdd: RDD[(Int, String)] = filteredRdd.map(x => (x.length, x))
+    val sortedRdd: RDD[(Int, String)] = pairRdd.sortByKey(ascending = true, numPartitions = 3).distinct()
+
+    sortedRdd.foreach(x => println(x))
+
+  }
+
+}
