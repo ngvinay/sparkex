b32e358ed3d1e2abc0535c552ff3a4ff777e9bd6
Sandeep KS <sandeep.ksetty@gmail.com>
2016-11-15 13:22:48 -0500

    Initial Functionality Check-in

--- src/com/dataworks/spark/rdd/pair/keysEx.scala
+++ src/com/dataworks/spark/rdd/pair/keysEx.scala
@@ -1,8 +1,29 @@
 package com.dataworks.spark.rdd.pair
 
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
 /**
   * Created by Sandeep on 10/28/16.
   */
 object keysEx {
+
+  def main(args: Array[String]): Unit = {
+    val conf: SparkConf = new SparkConf().setAppName("keysEx").setMaster("local")
+    val sc: SparkContext = new SparkContext(conf)
+    sc.setLogLevel("OFF")
+
+    keysExStr(sc)
+  }
+
+  def keysExStr(sc: SparkContext): Unit = {
+    val baseRDD: RDD[String] = sc.textFile("/Users/Sandeep/dataworks/data/spark")
+    val filteredRdd: RDD[String] = baseRDD.filter(line => !line.isEmpty).flatMap(_.split(" "))
+    val pairRdd: RDD[(Int, String)] = filteredRdd.map(x => (x.length, x))
+    val keys: RDD[Int] = pairRdd.keys.distinct()
+
+    keys.foreach(x => println(x))
+
+  }
 
 }
