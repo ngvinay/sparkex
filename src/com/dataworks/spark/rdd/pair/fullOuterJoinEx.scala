62acd785532a68917079aa77c91d1e13e2766039
Sandeep KS <sandeep.ksetty@gmail.com>
2016-11-21 14:16:37 -0500

    Initial Functionality Check-in

--- src/com/dataworks/spark/rdd/pair/fullOuterJoinEx.scala
+++ src/com/dataworks/spark/rdd/pair/fullOuterJoinEx.scala
@@ -1,8 +1,31 @@
 package com.dataworks.spark.rdd.pair
 
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
 /**
   * Created by Sandeep on 10/28/16.
   */
 object fullOuterJoinEx {
+  def main(args: Array[String]): Unit = {
+    val conf: SparkConf = new SparkConf().setAppName("fullOuterJoinEx").setMaster("local")
+    val sc: SparkContext = new SparkContext(conf)
+    sc.setLogLevel("OFF")
+
+    fullJoinEx(sc)
+  }
+
+  def fullJoinEx(sc: SparkContext): Unit = {
+    val thisRDD: RDD[String] = sc.textFile("/Users/Sandeep/dataworks/data/this")
+      .filter(x => !x.isEmpty)
+    val thatRDD: RDD[String] = sc.textFile("/Users/Sandeep/dataworks/data/that")
+      .filter(x => !x.isEmpty)
+
+    val thisPair: RDD[(Int, String)] = thisRDD.flatMap(x => x.split(" ")).map(x => (x.length, x))
+    val thatPair: RDD[(Int, String)] = thatRDD.flatMap(x => x.split(" ")).map(x => (x.length, x))
+
+    val fullOuterJoin: RDD[(Int, (Option[String], Option[String]))] = thisPair.fullOuterJoin(thatPair)
+    fullOuterJoin.foreach(x => println(x))
+  }
 
 }
